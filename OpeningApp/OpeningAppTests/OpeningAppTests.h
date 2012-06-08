7a71b906738c7ce00672fb69f92aae4760abfa0e
Robert DeNicola <thedizzle23@gmail.com>
2012-06-07 21:47:44 -0400

    half of it is good

---
+++ OpeningApp/OpeningAppTests/OpeningAppTests.h
@@ -0,0 +1,13 @@
+//
+//  OpeningAppTests.h
+//  OpeningAppTests
+//
+//  Created by Robert DeNicola on 6/7/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OpeningAppTests : SenTestCase
+
+@end
---
+++ OpeningApp/OpeningAppTests/OpeningAppTests.m
@@ -0,0 +1,32 @@
+//
+//  OpeningAppTests.m
+//  OpeningAppTests
+//
+//  Created by Robert DeNicola on 6/7/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "OpeningAppTests.h"
+
+@implementation OpeningAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OpeningAppTests");
+}
+
+@end
